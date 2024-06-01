f5595bb819f0a609157ab8619dc3c5898816a50f
Zhao Chang <76083130+cz-bszy@users.noreply.github.com>
2024-06-01 16:14:22 +0800

    Create this repository

---
+++ FC_Processing.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=4
+
+parallel_jobs=1
+
+working_dir="/mnt/c/Users/bszyc/Desktop/Test/fMRI" 
+tissuepriors_dir="/mnt/c/Users/bszyc/Desktop/Test/fMRI/Code/tissuepriors"
+standard_3mm="/mnt/c/Users/bszyc/Desktop/Test/fMRI/Code/standard"
+template_dir="/mnt/c/Users/bszyc/Desktop/Test/fMRI/Code/template"
+ls -d $working_dir/sub-* | xargs -n 1 basename > $working_dir/subjects.txt
+subjects_file="$working_dir/subjects.txt"
+
+num_threads=4
+FWHM=6
+sigma=2.54798709
+highp=0.1
+lowp=0.005
+TR=2
+TE=34
+vol=96
+fsf_type="Retain_GRS"
+
+mkdir -p "$working_dir/log"
+
+run_steps() {
+  subj=$1
+  echo "Processing subject: $subj ..."
+  
+  log_file="${working_dir}/log/${subj}_log.txt"
+  
+  {
+    echo "===== Running FC_step1 for subject: $subj ====="
+    #./FC_step1 $subj $num_threads $working_dir
+    echo "===== Completed FC_step1 for subject: $subj ====="
+    
+    echo "===== Running FC_step2 for subject: $subj ====="
+    ./FC_step2 $subj $num_threads $working_dir $FWHM $sigma $highp $lowp 
+    echo "===== Completed FC_step2 for subject: $subj ====="
+    
+    echo "===== Running FC_step3 for subject: $subj ====="
+    ./FC_step3 $subj $num_threads $working_dir $standard_3mm
+    echo "===== Completed FC_step3 for subject: $subj ====="
+    
+    echo "===== Running FC_step4 for subject: $subj ====="
+    ./FC_step4 $subj $num_threads $working_dir $sigma $tissuepriors_dir
+    echo "===== Completed FC_step4 for subject: $subj ====="
+    
+    echo "===== Running FC_step5 for subject: $subj ====="
+    ./FC_step5 $subj $num_threads $working_dir
+    echo "===== Completed FC_step5 for subject: $subj ====="
+    
+    echo "===== Running FC_step6 for subject: $subj ====="
+    bash ./FC_step6 $subj $num_threads $working_dir $template_dir $TR $TE $vol $fsf_type
+    echo "===== Completed FC_step6 for subject: $subj ====="
+
+  } &> $log_file
+}
+
+export -f run_steps
+
+export working_dir
+export tissuepriors_dir
+export template_dir
+export num_threads
+export FWHM
+export sigma
+export highp
+export lowp
+export TR
+export TE
+export vol
+export fsf_type
+export standard_3mm
+
+cat $subjects_file | parallel -j $parallel_jobs run_steps
+
+bash FC_step7 $subj $working_dir $fsf_type
